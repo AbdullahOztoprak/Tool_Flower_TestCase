c88e815f88e4ab6b6fe3eded4f3898cffacd1870
AbdullahOztoprak <e210503006@stud.tau.edu.tr>
2025-07-25 15:07:58 +0300

    Update test1.sh

--- test1.sh
+++ test1.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-# Print the server's IP address
-echo "Server IP address:"
-hostname -I
-
-# Save a test message to a file
-echo "Tool Flower test successful at $(date)" > test_output.txt
-echo "Test message written to test_output.txt"
+ip a > test.txt
+echo "Created test.txt with the output of 'ip a' command"
